72831cae3e7876631008dfcade64b1641e70ad81
Andrew Jones <Andrew.jones13@snhu.edu>
2023-05-26 20:01:46 -0400

    new file:   components/index.js 	modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components';
 
 const posts = [
   { title: 'Project One', excerpt: 'This is project one'},
@@ -14,17 +15,13 @@ export default function Home() {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => (
-            <div>
-              {post.title}
-              {post.excerpt}
-            </div>
-          ))}
+          {posts.map((post) => < PostCard post={post} key={post.title} />)}
         </div>
       </div>
       <div className='lg:col-span-4 col-span-1'>
         <div className='lg:sticky relative top-8'>
-          
+          <PostWidget />
+          <Categories />
         </div>
       </div>
 
