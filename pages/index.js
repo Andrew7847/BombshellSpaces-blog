e2e309ba8f873fe87736dc8e8a31eefb5ef98424
Andrew Jones <Andrew.jones13@snhu.edu>
2023-05-27 15:20:59 -0400

    Added initial query code

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 import Head from 'next/head';
 import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '@/services';
 
-const posts = [
-  { title: 'Project One', excerpt: 'This is project one'},
-  { title: 'Project Two', excerpt: 'This is project two'},
-];
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
@@ -26,4 +22,12 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
 }
