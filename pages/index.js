d25e2ed04edbc35dff95d95594c7185a70d5b31a
Andrew Jones <Andrew.jones13@snhu.edu>
2023-05-26 20:14:17 -0400

    A

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from 'next/head';
 import { PostCard, Categories, PostWidget } from '../components';
 
 const posts = [
@@ -8,23 +8,22 @@ const posts = [
 
 export default function Home() {
   return (
-    <div className='container mx-auto px-10 mb-8 bg-gray-300'>
+    <div className='container mx-auto px-10 mb-8'>
       <Head>
         <title>Bombshell Spaces</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post) => < PostCard post={post} key={post.title} />)}
+          {posts.map((post) => <PostCard post={post} key={post.title} />)}
+        </div>
+        <div className='lg:col-span-4 col-span-1'>
+          <div className='lg:sticky relative top-8'>
+            <PostWidget />
+            <Categories />
+          </div>
         </div>
       </div>
-      <div className='lg:col-span-4 col-span-1'>
-        <div className='lg:sticky relative top-8'>
-          <PostWidget />
-          <Categories />
-        </div>
-      </div>
-
     </div>
   )
 }
