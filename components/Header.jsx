0579d12daa8f2756ce5cd4d4414f6c40a87a3c5d
Andrew Jones <Andrew.jones13@snhu.edu>
2023-05-28 23:06:43 -0400

    Added author and PostDetail components

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,12 +5,12 @@ import { getCategories } from '@/services';
 
 const Header = () => {
   const [categories, setCategories] = useState([]);
-  
+
   useEffect(() => {
     getCategories()
       .then((newCategories) => setCategories(newCategories))
   }, []);
-    
+
   return (
     <div className='container mx-auto px-10 mb-8'>
       <div className='border-b w-full inline-block border-blue-400 py-8'>
