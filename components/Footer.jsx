4352614e252268f7a422c458627ac16a86e8969b
Andrew Jones <Andrew.jones13@snhu.edu>
2023-06-06 18:39:49 -0400

    Updated Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className='container mx-auto px-10 rounded-md bg-black bg-opacity-70'>
-      <span className='flex items-center justify-center xs:max-sm:text-xs text-white p-4'>
+      <span className='flex items-center justify-center text-xs sm:text-lg text-white p-4'>
         &copy; 2023 Copyright: 
         <Link href="/" className='ml-2'>BombshellSpaces.com</Link>
       </span>
